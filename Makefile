99171bb45d047915b1e791ec8e2872494e006aa8
jlm22010 <jonahmerriam2@gmail.com>
2025-09-15 13:52:13 -0400

    initial brute force attempt at logger.cpp

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 all:
 	c++ -Wall -Wextra -I ./include/ ./src/* -o test.out
 
+final:
+	c++ -Wall -Wextra -I ./include/ ./src/* -o logger
+
 clean:
 	rm *.out
--- src/logger.cpp
+++ src/logger.cpp
@@ -8,27 +8,86 @@ void addReading(const std::string& label, double value,
                 std::string labels[], double values[],
                 int& size, int capacity) {
     // TODO: throw "Full" if size == capacity, else insert and ++size
+	if(size == capacity){
+		throw "Full";
+		return;
+	}
+
+	// insert our values
+	labels[size] = label;
+	values[size] = value;
+
+	size++;
+
+	return;
 }
 
 void updateValue(double* valuePtr, double newValue) {
     // TODO: write through pointer
+	*valuePtr = newValue;
+
+	return;
 }
 
 void printReading(const std::string& label, const double& value) {
     // TODO: pretty-print one reading
+	std::cout << label << " has a value of [" << value << "]" << std::endl;
+
+	return;
 }
 
 double average(const double values[], int size) {
     // TODO: throw "Empty" if size==0, else compute average
-    return 0.0;
+	if(size == 0){
+		throw "Empty";
+
+		return 0.0;
+	}
+	
+	// compute average
+	double avg = 0.0;
+	for(int i = 0; i < size; i++){
+		avg += values[i];
+	}
+	avg /= size;
+
+	return avg;
 }
 
 double minValue(const double values[], int size) {
     // TODO: throw "Empty" if size==0, else compute min
-    return 0.0;
+	if(size == 0){
+		throw "Empty";
+
+		return 0.0;
+	}
+
+	// get the minimum by traversal
+	double min = values[0];
+	for(int i = 1; i < size; i++){
+		if(values[i] < min){
+			min = values[i];
+		}
+	}
+
+	return min;
 }
 
 double maxValue(const double values[], int size) {
     // TODO: throw "Empty" if size==0, else compute max
-    return 0.0;
+	if(size == 0){
+		throw "Empty";
+
+		return 0.0;
+	}
+	
+	// get the max by traversal
+	double max = values[0];
+	for(int i = 1; i < size; i++){
+		if(values[i] > max){
+			max = values[i];
+		}
+	}
+	
+	return max;
 }
